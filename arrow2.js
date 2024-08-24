bcfec823f86886448df092e8c1f4b14ccfbcc452
Nadeem-ashraf004 <nadeemmsaqi@gmail.com.com>
2024-08-24 19:27:31 +0500

    f

--- arrow2.js
+++ arrow2.js
@@ -3,3 +3,7 @@ const subtract = (a, b) => a - b;
 
 // Calling the function
 console.log(subtract(10, 4));
+const add =(a,b) =>a+b;
+console.log(add(2,7));
+const double=x=>x*2
+console.log(double(9))
