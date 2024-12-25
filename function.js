d8a467b1738082f27d2338a5c7c8dca118c0e042
Nadeem-ashraf004 <nadeemmsaqi@gmail.com.com>
2024-12-25 21:20:31 +0500

    start with basic

---
+++ function.js
@@ -0,0 +1,10 @@
+//function Decledaration
+function greet(name){
+    return 'Hello ,&{name }';
+}
+console.log(greet ('Alice','ali'));
+//function expression
+const add = function(a,b){
+    return a+b;
+}
+console.log(add(5,3));
