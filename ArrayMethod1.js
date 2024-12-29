4338f41fa46337fe7eb77a22a3fb3674712bfc82
Nadeem-ashraf004 <nadeemmsaqi@gmail.com.com>
2024-12-29 23:52:14 +0500

    add new file

---
+++ ArrayMethod1.js
@@ -0,0 +1,21 @@
+//length : if we want to find the lenght of any array then we use .lenght method
+const fruite=["apple" ,"orange" ,"mango"]
+let size =fruite.length;
+const city =["lahore","skardu","faisalabad","islamabad"]
+let siz=city.length;
+const emplyname=["nadeem","ashraf","umair","manzoor"]
+let name=emplyname.length
+console.log(size,siz,name)
+//tostring method
+const fruitee=["mango","orange","apple"]
+let change =fruitee.toString()
+console.log(change)
+//at()
+const namee=["nadeem","manzoor","shujaat"]
+let print=namee.at(1)
+console.log(print)
+//join()
+const nam=["nadeem","nadeem","nadeem"]
+let join=nam.join(" * ")
+console.log(join)
+//
--- function.js
+++ function.js
@@ -1,10 +1,19 @@
 //function Decledaration
-function greet(name){
-    return 'Hello ,&{name }';
-}
-console.log(greet ('Alice','ali'));
+ const greet = name => `Hello ,${name }!`;
+
+console.log(greet ('Alice'));
 //function expression
 const add = function(a,b){
     return a+b;
 }
 console.log(add(5,3));
+
+//arrow function
+const Add= (a,b)=>a+b
+console.log(Add(4,6))
+
+const mult=(a,b)=>a*b
+console.log(mult(5,9))
+
+const hell=()=>'hello world'
+console.log(hell());
